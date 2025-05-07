dbc6cc474308b7357f8fb1998dd625fbed6f92a3
halex22 <halex2209@gmail.com>
2025-05-07 13:27:16 +0200

    Add project files.

---
+++ SharpStudents/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace SharpStudents
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s1 = new() { 
+                Name = "Student A",
+                Surname = "Surname A",
+                Gender = "Peanut",
+                Yob = 600,
+                Marks = [10, 8, 6, 3]
+            };
+
+            Student s2 = new()
+            {
+                Name = "Student B",
+                Surname = "Surname B",
+                Gender = "Male",
+                Yob = 2000,
+                Marks = [9, 8, 5, 6]
+            };
+
+            Student s3 = new()
+            {
+                Name = "Student C",
+                Surname = "Surname C",
+                Gender = "Female",
+                Yob = 2001,
+                Marks = [10, 9, 8, 7]
+            };
+
+            List<Student> studentList = [s1, s2, s3];   
+
+
+            Console.WriteLine(s1.CalcolateMean());
+        }
+    }
+}
---
+++ SharpStudents/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpStudents
+{
+    internal class Student
+    {
+        private List<int> _marks = []; 
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Gender { get; set; }
+        public int Yob { get; set; }
+        public List<int>? Marks {
+            get => _marks;
+            set => _marks = value;
+        }
+
+        public void ToString() { }
+
+        public double CalcolateMean()
+        {
+            return this._marks.Sum() / (double) this._marks.Count;
+        }
+
+        
+
+    }
+}
